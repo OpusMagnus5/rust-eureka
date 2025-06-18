3769796ddd2b6909a63b19b8823dca34e66df6b4
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-06-18 17:59:13 +0200

    scope

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -35,3 +35,15 @@ fn blocks() {
         value * multiplier //działa podobnie jak funkcja i zwraca ostatni wynik linii, ale nie za pomocą return
     };
 }
+
+fn apply_to_jobs(number: i32, title: &str) {
+    println!("I'm applying to {number} {title} jobs")
+}
+
+fn is_even(number: i32) -> bool {
+    number % 2 == 0
+}
+
+fn alphabets(text: &str) -> (bool, bool) {
+    (text.contains('a'), text.contains('z'))
+}
