0c83df990c4cc341e4c57423c5cc29775a3e333c
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-06-18 17:30:17 +0200

    functions

---
+++ functions/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    open_store("Brooklyn", 32);
+    println!("Square 5 is {}",  square(5));
+    println!("Square 5 is {}", implicit_square(5));
+}
+
+fn open_store(neighborhood: &str, building_number: i32) {
+    println!("Opening my pizza store in {neighborhood} {building_number}");
+}
+
+fn square(number: i32) -> i32 {
+    return number * number;
+}
+
+// jeśli ostatnia linia zwraca wartości, możemy pominąć return i musimy usunąć ; na końcu
+fn implicit_square(number: i32) -> i32 {
+    number * number
+}
