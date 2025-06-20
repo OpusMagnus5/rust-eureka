70a71f05cf7b3fcca180c4bdc502813ec9ef825d
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-06-20 13:35:40 +0200

    ownership test

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -24,7 +24,7 @@
 
 
 
-fn main() {
+fn test() {
     // age jest ownerem value 33 i jest odpowiedzialny za zwolnienie pamięci po wyjściu z bloku
     // najpierw jest czyszczony is_handsome, a potem age zgodnie z LIFO
     let age: i32 = 33; 
@@ -241,7 +241,70 @@ fn add_sugar_2(mut meal: String) -> String {
     meal
 }
 
+/*
+Declare a `is_concert` variable set to a boolean.
+Declare a `is_event` variable assigned to `is_concert`.
+Will Rust move ownership? State your answer, then confirm
+by trying to printing both variables out.
+ 
+Declare a `sushi` variable to set to a string literal of "Salmon"
+Declare a `dinner` variable assigned to the `sushi` variable.
+Will Rust move ownership? State your answer, then confirm
+by trying to printing both variables out.
+ 
+Repeat the previous example but use a heap String instead.
+Will Rust move ownership? Explain why the result is different
+from the previous operation.
+ 
+The `clear` method modifies a heap String to have no content.
+Declare an `eat_meal` function that accepts a `meal` parameter
+of type String. In the body of `eat_meal`, invoke the `clear`
+method on the `meal` parameter.
+ 
+In the `main` function, invoke the `eat_meal` function and pass
+in your "Salmon" String. Explain what happens when the eat_meal
+function runs. Describe the complete movement of ownership of
+the "Salmon" String throughout the program.
+ 
+Say we want to keep the String around after `eat_meal` is
+called. How can we continue to have access to the String in
+the `main` function? Print out the (empty) String.
+*/
 
+fn main() {
+    // brak przeniesienia ownera, kopia wartości
+    let is_concert = true;
+    let is_event = is_concert;
+    println!("{is_concert}");
+    println!("{is_event}");
+
+    // brak przeniesienia ownera, kopia wartości
+    let sushi = "Salmon";
+    let dinner = sushi;
+    println!("{sushi}");
+    println!("{dinner}");
+
+    let sushi = String::from("Salmon");
+    let dinner = sushi;
+    // println!("{sushi}"); ERROR - owner przeniesiony do dinner
+    println!("{dinner}");
+    
+    /*
+     dinner zmienia ownera na meal w funkcji eat_meal, po zakończeniu funkcji meal jest utilizowany
+     a dinner jest INVALID
+    */
+    eat_meal(dinner);
+    // println!({dinner}); ERROR - owner przeniesiony do argumentu funkcji 
+    
+    /*
+     Gdybyśmy chcieli zatrzymać wartość po eat_meal, funkcja musiałaby zwrócić wartość czyli
+     przekazać ownera do dinner a dinner musiałaby być mutable.
+    */
+}
+
+fn eat_meal(mut meal: String) {
+    meal.clear()
+}
 
 
 
