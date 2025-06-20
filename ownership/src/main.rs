1e92c9680ee20679c1eeced49c6b82a02d182cd1
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-06-20 10:14:45 +0200

    ownership

---
+++ ownership/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    /*
+    ownership - to feature kompilatora, nie ma impaktu w runtime działającego programu
+    To zestaw reguł kompilatora, który sprawdza, czy program jest wolny od błędów pamięci
+    
+    Każda wartość w Rust ma ownera, owner wartości może się zmieniać w trakcie działania programu, ale
+    zawsze jest tylko jeden w danym momencie.
+    
+    Ownerem może być zmienna albo parametr funkcji. Zmienna może być ownerem tuple, ale tuple jest
+    ownerem wartości w swojej kolekcji.
+    */
+}
