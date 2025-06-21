db9c02c72e58a6eb3a8eee78a9beec0d5847c7a3
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-06-21 17:02:51 +0200

    create struct instance

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -8,11 +8,19 @@
  
 */
 fn define_struct() {
+    // definiowanie typu
     struct Coffee {
         price: f64,
         name: String,
         is_hot: bool,
     }
+    
+    // tworzenie instancji typu
+    let mocha = Coffee {
+        name: String::from("Mocha"),
+        price: 4.99,
+        is_hot: true
+    };
 }
 
 fn main() {
