28fca9a3b3698fdf7bba5242a617f88e88d9b31b
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-06-21 16:59:48 +0200

    define struct

---
+++ structs/src/main.rs
@@ -0,0 +1,20 @@
+
+/*
+ Struct - obiekt w Java
+ Rust ma 3 typy stucts:
+    * named field structs
+    * tuple-like structs
+    * unit-like structs
+ 
+*/
+fn define_struct() {
+    struct Coffee {
+        price: f64,
+        name: String,
+        is_hot: bool,
+    }
+}
+
+fn main() {
+    
+}
