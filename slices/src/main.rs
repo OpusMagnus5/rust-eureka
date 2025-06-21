278409d9890e2fe4ad5a867d1af56f8bcf92002b
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-06-21 11:16:51 +0200

    slices

---
+++ slices/src/main.rs
@@ -0,0 +1,10 @@
+fn slices() {
+    /*
+     Slice jest referencją do części (może być również to cała kolekcja) jakiejś kolekcji / tablicy / tuple / stringa
+     Jako referencja Slice nie zabiera ownera kolekcji.
+    */
+}
+
+fn main() {
+    
+}
