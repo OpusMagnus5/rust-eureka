a9d1fd932300188409e00e7a014c712922753f55
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-06-22 19:05:50 +0200

    struct test

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -290,9 +290,91 @@ fn unit_struct() {
     let my_empty_struct = Empty; // inicjalizacja
 }
 
+// ============================================================================================== //
+
+/*
+Define a Flight struct with the following fields:
+  - an `origin` field (String)
+  - a `destination` field (String)
+  - a `price` field (f64)
+  - a `passengers` field (u32)
+ 
+Derive a Debug trait implementation for the Flight struct.
+ 
+Define a `new` constructor function that returns a new
+instance of a Flight.
+ 
+Define a `change_destination` method that accepts a new
+destination and overwrites the value of the `destination`
+field.
+ 
+Define a `increase_price` method that raises the value
+of the `price` by 20% (multiply the `price` field by 1.20).
+Make sure to save the new `price` field value.
+ 
+Define a `itinerary` method that prints out both the
+`origin` and `destination` fields in the following format
+(origin -> destination).
+ 
+Use the constructor function to create a new Flight instance
+in the main function. Invoke all of the defined methods.
+Print out the struct in Debug format to confirm the struct
+updates as you expect.
+ 
+Use struct update syntax to copy the `price` and `passengers`
+fields to a new Flight struct instance. Make sure to provide
+new Strings for the remaining fields to ensure ownership
+doesn't transfer. Assign the new Flight to a separate variable.
+*/
+
+#[derive(Debug)]
+struct Flight {
+    origin: String,
+    destination: String,
+    price: f64,
+    passengers: u32
+}
+
+impl Flight {
+    fn new(origin: String, destination: String, price: f64, passengers: u32) -> Self {
+        Self {
+            origin,
+            destination,
+            price,
+            passengers
+        }
+    }
+    
+    fn change_destination(&mut self, new_destination: String) {
+        self.destination = new_destination;
+    }
+    
+    fn increase_price(&mut self) {
+        self.price *= 1.2
+    }
+    
+    fn itinerary(&self) {
+        println!("{} -> {}", self.origin, self.destination);
+    }
+}
+
+fn test() {
+    let mut flight =  Flight::new(String::from("Katowice"), String::from("Malta"), 297.46, 64);
+    println!("{flight:#?}");
+    flight.change_destination(String::from("Alicante"));
+    flight.increase_price();
+    flight.itinerary();
+    println!("{flight:#?}");
+    
+    let new_flight = Flight {
+        origin: String::from("Kraków"),
+        destination: String::from("London"),
+        ..flight
+    };
+}
 
 fn main() {
-    
+    test();
 }
 
 
