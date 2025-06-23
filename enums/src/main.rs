11a85ab752de5ad26fd8f68bb87c1b03cc0f1851
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-06-23 18:54:56 +0200

    enums - tuple variant

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -12,6 +12,50 @@ fn intro_to_enums() {
 
 // ============================================================================================== //
 
+#[derive(Debug)]
+enum PaymentMethodType {
+    CreditCard(String), // możemy podać wiele typów wartości, przypomina tuple
+    DebitCard(String),
+    PayPal(String, String), // ilość wartości nie musi się zgadzać
+}
+
+/*
+ Rust wybierze rozmiar pamięci dla wszystkich Enumów na podstawie tego z największą ilością zajmowanego przez niego
+ i jego zmienne miejsca.
+*/
+
+fn enums_with_associated_values() {
+    /*
+     Nie przypomina to Javy gdzie wartość przypisana do pola enuma jest stała.
+    */
+    let mut my_payment_method: PaymentMethodType = PaymentMethodType::CreditCard(String::from("0100-4528"));
+    my_payment_method = PaymentMethodType::PayPal(String::from("bob@gmail.com"), String::from("abc123"));
+}
+
 fn main() {
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
