2e70587e2a9c75133f97ac4633605f9206574dcb
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-06-23 18:33:07 +0200

    intro to enums

---
+++ enums/src/main.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+enum CardSuit {
+    Hearts,
+    Diamonds,
+    Spades,
+    Clubs,
+}
+
+fn intro_to_enums() {
+    let first_card = CardSuit::Hearts; // inicjalizacja enuma
+}
+
+// ============================================================================================== //
+
+fn main() {
+    
+}
