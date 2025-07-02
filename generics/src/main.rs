bd637a50da1122ad78018aa349a775ce90a28899
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-07-02 17:57:51 +0200

    turbofish operator

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -3,13 +3,15 @@
  T - może być to dowolna litera bądź nawet nazwa
  Pod spodem tak naprawdę nie ma żadnego mechanizmu generyczności. W wynikowym pliku dla każdego
  typu użytego w funkcji zostaje rozpisana funkcja przy użyciu danego typu.
+ 
+ Turbofish operator pozwala nadpisać defaultowy typ
 */
 fn identity<T>(value: T) -> T {
     value
 }
 
 fn intro_to_generics() {
-    println!("{}", identity(5));
+    println!("{}", identity::<i64>(5)); // nadpisujemy i32 przez i64
     println!("{}", identity(5.77));
 }
 
