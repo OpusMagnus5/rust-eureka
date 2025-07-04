e7861c6107547d383a63540d4bdfdb32f8c6fddb
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-07-04 13:22:05 +0200

    return result enum

--- option-and-result-enums/src/main.rs
+++ option-and-result-enums/src/main.rs
@@ -157,6 +157,25 @@ fn result_enum_real_example() {
     println!("{text_as_number:?}");
 }
 
+// ============================================================================================== //
+
+fn divide(numerator: f64, denominator: f64) -> Result<f64, String> {
+    if denominator == 0.0 { 
+        Err("Cannot divide by zero.".to_string())
+    } else { 
+        Ok(numerator / denominator)
+    }
+}
+
+fn return_result_enum() {
+    let result = divide(10.0, 2.0);
+    
+    match result { 
+        Ok(calculation) => println!("Result: {calculation}"),
+        Err(message) => println!("Error: {message}")
+    }
+}
+
 fn main() {
     
 }
