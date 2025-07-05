e64cdd527e4d2244839bea9c74178db7bb1071aa
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-07-05 10:15:49 +0200

    vector - adding and removing elements

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -12,6 +12,22 @@ fn create_vector() {
     let pizza_diameters = vec![8, 10, 12]; // makro z możliwością inicjalizacji wartościami
 }
 
+// ============================================================================================== //
+
+fn adding_removing_elements() {
+    /*
+     Aby dodać lub usunąć coś z vectora musimy oznaczyć go jako mut
+    */
+    let mut pizza_diameters = vec![8, 10, 12];
+    pizza_diameters.push(14); // dodaje element na koniec
+    /*
+     dodajemy na konkretną pozycję, reszta wartości się przesuwa w prawo - nie podmienia wartości
+    */
+    pizza_diameters.insert(0, 6);
+    let last_diameter: Option<i32> = pizza_diameters.pop(); // usuwa i zwraca ostatni element
+    let removed_diameter: i32 = pizza_diameters.remove(2); // usuwa i zwraca element na danym indeksie
+}
+
 fn main() {
 
 }
