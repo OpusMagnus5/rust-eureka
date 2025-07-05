6a7cd4cb7cc62e43ae37395cee9eb79b618b74f9
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-07-05 10:26:59 +0200

    vector - reading elements

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,5 +1,5 @@
 /*
- Vector w przeciwieństwie to tablicy może zmieniać rozmiar w czasie działania programu a dane są 
+ Vector w przeciwieństwie to tablicy może zmieniać rozmiar w czasie działania programu a dane są
  połączone ze sobą w kolejności zarówno semantycznej jak i w pamięci komputera
 */
 
@@ -28,6 +28,59 @@ fn adding_removing_elements() {
     let removed_diameter: i32 = pizza_diameters.remove(2); // usuwa i zwraca element na danym indeksie
 }
 
+// ============================================================================================== //
+
+fn reading_elements() {
+    let pizza_diameters = vec![8, 10, 12, 14];
+    
+    let pepperoni = String::from("Pepperoni");
+    let mushroom = String::from("Mushroom");
+    let sausage = String::from("Sausage");
+    let pizza_toppings: Vec<String> = vec![pepperoni, mushroom, sausage]; // vector przejmuje ownera wartości
+    
+    let value: i32 = pizza_diameters[2]; // i32 wspiera Copy więc nie przejmuje ownera
+    // let topping_value = pizza_toppings[2]; // to nie zadziała bo String nie wspiera Copy
+    let reference = &pizza_toppings[2]; // OK
+    
+    let pizza_slice: &[String] = &pizza_toppings[1..3]; // tworzy referencje do sekcji vectora
+}
+
 fn main() {
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
