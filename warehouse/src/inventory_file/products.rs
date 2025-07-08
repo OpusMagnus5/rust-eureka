c163a8507978882f06b4b97137f0b4ee9ab6db7b
Damian Bodzioch <damianbodzioch95@gmail.com>
2025-07-08 11:39:27 +0200

    self keyword

--- warehouse/src/inventory_file/products.rs
+++ warehouse/src/inventory_file/products.rs
@@ -10,3 +10,14 @@ pub struct Item {
     pub category: ProductCategory,
     pub quantity: u32
 }
+
+impl Item {
+    pub fn new(name: String, category: ProductCategory, quantity: u32) -> Self { // musi byc pub jeśli chcemy dostęp z zewnątrz
+        super::talk_to_manager(); // sięgamy do rodzica za pomocą super
+        Self {
+            name,
+            category,
+            quantity
+        }
+    }
+}
