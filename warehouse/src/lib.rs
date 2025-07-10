233cbfea5dd2d7c43e9945349587ef504143c2f7
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-07-10 20:00:10 +0200

    test

--- warehouse/src/lib.rs
+++ warehouse/src/lib.rs
@@ -3,3 +3,35 @@ pub mod orders;
 
 pub use inventory_file::{ Item, ProductCategory, FLOOR_SPACE, MANAGER as INVENTORY_MANAGER };
 pub use orders::MANAGER as ORDERS_MANAGER;
+
+// ============================================================================================== //
+
+pub mod diet {
+    const NUTRITIONIST: &str = "Norah Nutrition";
+    pub fn ask_about_program() {
+        println!("The nutritionist is {NUTRITIONIST}")
+    }
+}
+
+pub mod weightlifting;
+pub mod cardio;
+
+pub use cardio::CardioTool;
+pub use cardio::Exercise as CardioExercise;
+pub use weightlifting::Exercise as WeightliftingExercise;
+
+#[derive(Debug)]
+pub struct GymWorkout {
+    cardio: CardioExercise,
+    weightlifting: WeightliftingExercise
+}
+
+impl GymWorkout {
+    pub fn new(cardio: CardioExercise, weightlifting: WeightliftingExercise) -> GymWorkout {
+        diet::ask_about_program();
+        cardio::ask_about_program();
+        weightlifting::ask_about_program();
+        
+        GymWorkout { cardio, weightlifting }
+    }
+}
