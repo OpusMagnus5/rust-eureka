dfbca2e587731c4162dd3450cab63b4f03e94c6e
Damian Bodzioch <damianbodzioch95@gmail.com>
2025-07-14 15:40:18 +0200

    common string methods

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -31,6 +31,23 @@ fn format_macro() {
     println!("{}", full_name);
 }
 
+// ============================================================================================== //
+
+fn string_methods() {
+   let mut music_genres = "   Rock, Metal, Country, Rap    ";
+    println!("{}", music_genres.trim());
+    println!("{}", music_genres.trim_start());
+    println!("{}", music_genres.trim_end());
+
+    music_genres = music_genres.trim();
+    println!("{}", music_genres.to_uppercase());
+    println!("{}", music_genres.to_lowercase());
+    println!("{}", music_genres.replace("Rock", "Jazz"));
+
+    let genres: Vec<&str> = music_genres.split(", ").collect();
+    println!("{:?}", genres);
+}
+
 fn main() {
-    format_macro();
+    string_methods();
 }
