539121110cd155b4ef33f0966ce7578a4f2c8b26
Damian Bodzioch <damianbodzioch95@gmail.com>
2025-07-14 15:58:17 +0200

    strings test

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -60,8 +60,83 @@ fn user_input() {
     println!("Hello, {}", name);
 }
 
+// ============================================================================================== //
+
+/*
+Define a `make_money` function that accepts a mutable
+String reference. The function should concatenate
+the characters "$$$" to the end of the String.
+Invoke the function in `main`.
+
+Define a `trim_and_capitalize` function that accepts
+a string slice. It should return a String with
+all whitespace removed and all characters in uppercase.
+Invoke the function in `main`.
+
+Define an `elements` function that accepts a string
+slice. It should split the string by all occurrences
+of the `!` symbol and return a vector of the string
+slices. Invoke the function in `main`.
+
+Example:
+elements("Gold!Silver!Platinum")
+=> Vector of ["Gold", "Silver", "Platinum"]
+
+Define a `get_identity` function. The function should
+ask the user for their first and last name in TWO
+steps (i.e., collect user input twice). Make sure
+to communicate the instructions to the user.
+For each Result enum you receive, call the `expect`
+method and provide a custom error message (like
+"Failed to collect first name"). Return a String
+with the first and last names combined. Invoke
+the `get_identity` function in `main`, and output the
+returned String.
+
+Example:
 fn main() {
-    user_input();
+  let name = get_identity();
+   println!("{name}"); // Bill Murray
+}
+*/
+
+fn test() {
+    let mut money = String::from("1000");
+    make_money(&mut money);
+    println!("{}", money);
+
+    println!("{}", trim_and_capitalize("   hello world   "));
+
+    println!("{:?}", elements("Gold!Silver!Platinum"));
+
+    println!("{}", get_identity());
+}
+
+fn make_money(money: &mut String) {
+    money.push_str("$$$");
+}
+
+fn trim_and_capitalize(text: &str) -> String {
+    text.trim().to_uppercase()
+}
+
+fn elements(text: &str) -> Vec<&str> {
+    text.split("!").collect()
+}
+
+fn get_identity() -> String {
+    println!("Please enter your first name");
+    let mut first_name = String::new();
+    io::stdin().read_line(&mut first_name).expect("Failed to collect input");
+    println!("Please enter your last name");
+    let mut last_name = String::new();
+    io::stdin().read_line(&mut last_name).expect("Failed to collect input");
+
+    format!("{} {}", first_name.trim(), last_name.trim())
+}
+
+fn main() {
+    test();
 }
 
 
