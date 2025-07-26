94c2cac34ba335356768303e71f65456a294ad4c
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-07-26 08:04:23 +0200

    overriding value

--- hash-maps/src/main.rs
+++ hash-maps/src/main.rs
@@ -60,6 +60,17 @@ fn access_value_by_key() {
     let value = option_value.copied(); // tu jest kopia wartości ale nadal option
 }
 
+// ============================================================================================== //
+
+fn overriding_value() {
+    let mut coffee_pairings: HashMap<&str, &str> = HashMap::new();
+    let drink = String::from("Latte");
+    let milk = String::from("Oat Milk");
+    coffee_pairings.insert(&drink, &milk);
+    
+    coffee_pairings.insert("Latte", "Pistachio Milk"); // nadpisanie wartości pod kluczem
+}
+
 fn main() {
     ownership();
 }
