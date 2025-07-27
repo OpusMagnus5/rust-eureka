52380e9169b022cd7e4bdf24286cd7d30b1cf3fe
Damian Bodzoch <damianbodzioch95@gmail.com>
2025-07-27 13:30:16 +0200

    asking user for input

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -6,7 +6,7 @@ fn panic_macro() {
      Recoverable to błędy które możemy obsłużyć w kodzie
      Unrecoverable to błędy które powoduję wywalenie się programu
     */
-    
+
     panic!("Something went wrong"); // rzuca błąd
 }
 
@@ -18,7 +18,7 @@ fn process_module_and_exit_function() {
     /*
      Aby zamknąć program bez błędów używamy funkcji exit z modułu process
     */
-    
+
     process::exit(0); // zamyka program z kodem, 0 - brak błędu
 }
 
@@ -37,16 +37,76 @@ fn standard_error() {
 use std::fs::File;
 
 fn opening_file() {
-    let file = match File::open("story1.txt") { // otwiera plik i zwraca enum
+    let file = match File::open("story.txt") { // otwiera plik i zwraca enum
         Ok(file) => file,
         Err(error) => {
             eprintln!("Something went wrong reading the file. The error was {error:?}");
             process::exit(1);
         }
-    }; 
+    };
+    println!("{file:?}");
+}
+
+// ============================================================================================== //
+
+use std::io::stdin;
+
+fn user_input() {
+    println!("Please enter the name of the file you'd like to read:");
+    let mut input = String::new();
+    
+    let user_requested_file = stdin().read_line(&mut input); // odczytanie linii
+    
+    if let Err(error) = user_requested_file {
+        eprintln!("Something went wrong collecting user input. The error was {error}");
+        process::exit(1);
+    }
+
+    let file = match File::open(input.trim()) {
+        Ok(file) => file,
+        Err(error) => {
+            eprintln!("Something went wrong reading the file. The error was {error:?}");
+            process::exit(1);
+        }
+    };
     println!("{file:?}");
 }
 
 fn main() {
-    opening_file();
+    user_input();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
